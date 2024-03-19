ca12b31bf2c11819914f5d4ac928af407576be40
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-03-19 09:35:24 +0900

    save

--- wasm-sb-bindgen-testcode/src/lib.rs
+++ wasm-sb-bindgen-testcode/src/lib.rs
@@ -14,7 +14,6 @@ pub fn nya(t: String) -> String {
     ("nya(=^・・^=) {}".to_string() + &t).clone()
 }
 
-
 #[wasm_sb_bindgen]
 pub fn nya_2(t: String) -> String {
     // <wasm_bindgen::convert::ReturnWasmAbi>::return_abi(Ok(1));
@@ -22,6 +21,17 @@ pub fn nya_2(t: String) -> String {
     ("nya(=^・・^=) {}".to_string() + &t).clone()
 }
 
+#[wasm_sb_bindgen]
+pub fn woof(t: String, n: usize) -> String {
+    format!("woof(=^・・^=) {} {}", t, n)
+}
+
+// #[wasm_sb_bindgen]
+// pub fn kow<T>(t: T) -> String where
+//     T: WasmAbi + Debug + Clone + FromWasmAbi
+// {
+//     format!("woof(=^・・^=) {:?}", t)
+// }
+
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
